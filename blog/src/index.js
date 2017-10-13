0f05a5891c63498dba0236a073db7e74f3f44f3f
Rathi <github email address>
2017-10-13 11:55:16 -0400

    all the basic set up done to start magic

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
+import reducers from './reducers';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
